8f47175b77b58189ba4a3f79deff6f824a30a43a
CiroMarra <tensha93@gmail.com>
2024-05-23 16:38:30 +0200

    first commit

---
+++ database/seeders/ComicsSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\comic;
+
+class ComicsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $comicArray = config('comic');
+
+        foreach($comicArray as $comicBook) {
+            $newComic = new Comic();
+            $newComic->title = $comicBook['title'];
+            $newComic->description = $comicBook['description'];
+            $newComic->type = $comicBook['type'];
+            $newComic->price = $comicBook['price'];
+            $newComic->series = $comicBook['series'];
+            $newComic->sale_date = $comicBook['sale_date'];
+            $newComic->thumb = $comicBook['thumb'];
+            $newComic->save();
+    }
+    
+}
+}
