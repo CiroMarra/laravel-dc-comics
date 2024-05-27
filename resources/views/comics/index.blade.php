4d5482765a24e69a0c3fdcab2202bd1345aa8e38
CiroMarra <tensha93@gmail.com>
2024-05-27 14:24:51 +0200

    feat: add a destroy CRUD

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,13 +10,23 @@
                     <div class="col">
                         <div class="card my-3">
                             <img src="{{ $comic->thumb}}">
-                            <div class="card-body">
-                            <h5 class="card-title">{{ $comic->title }}</h5>
+                            <div class="card-body my-3"  style="height: 16rem">
+                            <h5>{{ $comic->title }}</h5>
                             <div>Prezzo: {{ $comic->price }} euro.</div>
                             <div>Serie: {{ $comic->series }}</div>
                             <div>Data di rilascio: {{ $comic->sale_date}}</div>
-                            <a href="{{ route('comics.show', ['comic' =>$comic->id]) }}" class="btn btn-primary">Scopri di più</a>
-                            <a href="{{ route('comics.edit', ['comic' =>$comic->id]) }}" class="btn btn-primary">Edita</a>
+                            <div class="my-4">
+                                <a href="{{ route('comics.show', ['comic' =>$comic->id]) }}" class="btn btn-primary">Scopri di più</a>
+                                <a href="{{ route('comics.edit', ['comic' =>$comic->id]) }}" class="btn btn-primary">Edita</a>
+                            </div>
+                            <div class="py-1">
+                                <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+
+                                    <button class="btn btn-danger js-delete-btn" data-comic-title="{{ $comic->title }}" type="submit">Elimina</button>
+                                </form>
+                            </div>
                             </div>
                         </div>
                     </div>
