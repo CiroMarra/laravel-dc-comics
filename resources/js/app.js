d461bec265ff751b3a11711cffc0005fdf3c356b
CiroMarra <tensha93@gmail.com>
2024-05-27 15:03:12 +0200

    feat: add bonus - modal

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,3 +4,22 @@ import * as bootstrap from 'bootstrap';
 import.meta.glob([
     '../img/**'
 ])
+
+
+const allDeleteComics = document.querySelectorAll('.js-delete-btn');
+allDeleteComics.forEach((deleteButton) => {
+    deleteButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        const deleteModal = document.getElementById('deleteModal');
+        const comicTitle = this.dataset.comicTitle;
+        deleteModal.querySelector('.modal-body').innerHTML = `Sei sicuro di voler eliminare dalla lista dei fumetti ${comicTitle}?`;
+
+        const bootstrapDeleteModal = new bootstrap.Modal(deleteModal);
+        bootstrapDeleteModal.show();
+
+        const modalDelate = document.getElementById('modal-confirm-deletion');
+       modalDelate.addEventListener('click', function() {
+            deleteButton.parentElement.submit();
+        });
+    });
+});
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -10,22 +10,39 @@
                     <div class="col">
                         <div class="card my-3">
                             <img src="{{ $comic->thumb}}">
-                            <div class="card-body my-3"  style="height: 16rem">
+                            <div class="card-body my-1"  style="height: 17rem">
                             <h5>{{ $comic->title }}</h5>
                             <div>Prezzo: {{ $comic->price }} euro.</div>
                             <div>Serie: {{ $comic->series }}</div>
                             <div>Data di rilascio: {{ $comic->sale_date}}</div>
-                            <div class="my-4">
+                            <div class="my-2">
                                 <a href="{{ route('comics.show', ['comic' =>$comic->id]) }}" class="btn btn-primary">Scopri di più</a>
                                 <a href="{{ route('comics.edit', ['comic' =>$comic->id]) }}" class="btn btn-primary">Edita</a>
                             </div>
-                            <div class="py-1">
+                            <div class="my-1">    
                                 <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="POST">
                                     @csrf
                                     @method('DELETE')
 
                                     <button class="btn btn-danger js-delete-btn" data-comic-title="{{ $comic->title }}" type="submit">Elimina</button>
                                 </form>
+                            </div>
+
+                            <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="deleteModalLabel">Conferma eliminazione</h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                        <button type="button" id="modal-confirm-deletion" class="btn btn-danger">Elimina</button>
+                                    </div>
+                                </div>
+                                </div>
                             </div>
                             </div>
                         </div>
